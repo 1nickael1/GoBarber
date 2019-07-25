919cf6e0d04ab0526d8935cc6ccfbb62fc97aea2
1nickael1 <qaznickael@hotmail.com>
2019-07-25 03:14:28 -0300

    Update gitignore

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '470ca00b54d4eb',
+    pass: '0d838a668bf656',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
